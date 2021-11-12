e9a8e5ba39133a4d638cb2901626b547f41c2325
Norucova <tu201906017@code.edu.az>
2021-11-12 09:42:03 +0400

    first commit

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = PrintAll();
+            Console.ReadKey();
+        }
+        public static async Task PrintAll()
+        {
+            for (int i = 0; i <10; i++)
+            {
+                    await PrintA();
+                    await PrintB();
+            }
+
+
+        }
+        public static async Task PrintA()
+        {
+            await Task.Run(() =>
+            {
+
+                for (int i = 0; i <2; i++)
+                {
+                    Console.WriteLine("A");
+                    Task.Delay(50).Wait();
+
+                }
+            });
+        }
+        public static async Task PrintB()
+        {
+            await Task.Run(() =>
+            {
+                    Console.WriteLine("B");
+                    Task.Delay(20).Wait();
+            });
+        }
+    }
+
+}
